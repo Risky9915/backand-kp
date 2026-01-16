d7ae09d033bf9eec1b1282160300a3285b75c5ff
Risky9915 <Nadiasafitri12112@gmail.com>
2026-01-17 02:51:05 +0700

    fix listen port for railway

--- src/server.js
+++ src/server.js
@@ -6,6 +6,6 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server berjalan di http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server berjalan di port ${PORT}`);
 });
