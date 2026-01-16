b32f5af214a741baf29378e1c0b9aa998fa51dbb
Risky9915 <Nadiasafitri12112@gmail.com>
2026-01-17 05:52:25 +0700

    fix start script to use src/server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,10 @@
 import app from './app.js';
 import dotenv from 'dotenv';
-import './config/db.js';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`🚀 Server berjalan di port ${PORT}`);
 });
