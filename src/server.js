4c591071a16268d00e3c0180199cc09cfbf771be
Risky9915 <Nadiasafitri12112@gmail.com>
2026-01-19 00:28:22 +0700

    Fix serverless setup for Railway

--- src/server.js
+++ src/server.js
@@ -1,10 +1,3 @@
 import app from './app.js';
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server berjalan di port ${PORT}`);
-});
+export default app;
