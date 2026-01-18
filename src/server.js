4d2e8027dd403c4542bbfb46e6770ac09ab0e572
Risky9915 <Nadiasafitri12112@gmail.com>
2026-01-19 02:19:36 +0700

    Fix server to Railway PORT

--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ import app from './app.js';
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`🚀 Backend berjalan di port ${PORT}`);
 });
